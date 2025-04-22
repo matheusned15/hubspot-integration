4d29bd2cf5644532653e6481ae5e560a333c5ac4
matheusned15 <matheusned15@gmail.com>
2025-04-21 23:50:29 -0300

    Projeto base e configuração inicial

---
+++ src/main/java/com/example/hubspot/HubspotIntegrationApplication.java
@@ -0,0 +1,11 @@
+package com.example.hubspot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HubspotIntegrationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HubspotIntegrationApplication.class, args);
+    }
+}
